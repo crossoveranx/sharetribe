c3a939229e4a5a40e46b635612761e3f9cab8b0d
James Lawton <crossoveranx@gmail.com>
2019-11-01 20:41:19 +0100

    added aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,2 +1,2 @@
-AWS.config(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+Aws.config(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
