46266a4bf4d739d9aa00f32959356b98eb9abf3e
James Lawton <crossoveranx@gmail.com>
2019-11-01 20:45:30 +0100

    added aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,2 +1,4 @@
-Aws.config(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+Aws.config.update({
+ region: 'eu-central-1',
+ credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+})
